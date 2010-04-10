400d0a6e4b9a4dcc6e13f613f0540a4f66a641c3
Adrian L Lange <adrianlund@gmail.com>
2010-04-10 02:18:13 +0200

    Allow some movement

--- Yatzee.lua
+++ Yatzee.lua
@@ -9,8 +9,17 @@ this addon without express, written permission of the author.
  
 --]]
 
-local function Update(self)
-	self:SetValue(GetLootRollTimeLeft(self.id))
+local active = false
+local configuring = false
+local frames = {}
+
+local function Update(self, elapsed)
+	if(not configuring) then
+		self:SetValue(GetLootRollTimeLeft(self.id))
+	else
+		self.dummyTime = self.dummyTime - elapsed
+		self:SetValue(self.dummyTime)
+	end
 end
 
 local function IconClick(self)
@@ -61,12 +70,6 @@ local function MODIFIER_STATE_CHANGED(self)
 	end
 end
 
-local function CANCEL_LOOT_ROLL(self, id)
-	if(id == self.id) then
-		self:Hide()
-	end
-end
-
 local function OnShow(self)
 	local texture, name, _, _, bop, needable, _, nukable, reason = GetLootRollItemInfo(self.id)
 	self.icon:SetNormalTexture(texture)
@@ -88,6 +91,11 @@ local function OnShow(self)
 
 	self:SetMinMaxValues(0, self.time)
 	self:SetValue(self.time)
+
+	self:SetMovable(false)
+	self.icon:EnableMouse(true)
+	self.need:EnableMouse(true)
+	self.greed:EnableMouse(true)
 end
 
 local function OnEvent(self, event, id)
@@ -95,14 +103,22 @@ local function OnEvent(self, event, id)
 		if(id == self.id) then
 			self:Hide()
 		end
+
+		for index, frame in pairs(frames) do
+			if(frame:IsShown()) then
+				active = true
+				return
+			else
+				active = false
+			end
+		end
 	else
 		MODIFIER_STATE_CHANGED(self)
 	end
 end
 
-local frames = {}
 for index = 1, 4 do
-	local bar = CreateFrame('StatusBar', nil, UIParent)
+	local bar = CreateFrame('StatusBar', 'Yatzee'..index, UIParent)
 	bar:SetSize(300, 10)
 	bar:SetBackdrop({bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=], insets = {top = -1, bottom = -1, left = -1, right = -1}})
 	bar:SetBackdropColor(0, 0, 0)
@@ -111,15 +127,19 @@ for index = 1, 4 do
 	bar:SetScript('OnUpdate', Update)
 	bar:SetScript('OnMouseUp', ButtonClick)
 	bar:SetScript('OnEvent', OnEvent)
-	bar:SetScript('OnShow', OnShow)
 	bar:EnableMouse(true)
 	bar:RegisterEvent('CANCEL_LOOT_ROLL')
 	bar:RegisterEvent('MODIFIER_STATE_CHANGED')
 	bar.type = 0
 	bar:Hide()
 
+	bar:SetMovable(true)
+	bar:SetUserPlaced(true)
+	bar:SetClampedToScreen(true)
+	bar:CreateTitleRegion():SetAllPoints()
+
 	local background = bar:CreateTexture('$parentBG', 'BORDER')
-	background:SetAllPoints(bar)
+	background:SetAllPoints()
 	background:SetTexture(1/3, 1/3, 1/3)
 
 	local icon = CreateFrame('Button', nil, bar)
@@ -170,21 +190,92 @@ for index = 1, 4 do
 	frames[index] = bar
 end
 
+local function SlashCommand(str)
+	if(InCombatLockdown() or active) then
+		return print('|cffff8080Yatzee:|r Cannot alter the frames right now, try again later')
+	elseif(str == 'reset') then
+		for index, frame in pairs(frames) do
+			frame:ClearAllPoints()
+			if(index == 1) then
+				frame:SetPoint('CENTER', 300, 100)
+			else
+				frame:SetPoint('BOTTOMLEFT', frames[index - 1], 'TOPLEFT', 0, 26)
+			end
+		end
+	else
+		for index, frame in pairs(frames) do
+			if(not frame:IsShown()) then
+				frame.icon:SetNormalTexture([=[Interface\Icons\INV_Misc_CandySkull]=])
+				frame.icon:GetNormalTexture():SetTexCoord(0.08, 0.92, 0.08, 0.92)
+
+				frame.name:SetFormattedText('Yatzee Frame #%s', index)
+				frame:SetWidth(frame.name:GetWidth() + 100)
+
+				frame.nukable = true
+				MODIFIER_STATE_CHANGED(frame)
+
+				GroupLootFrame_EnableLootButton(frame.need)
+				frame.need.reason = nil
+
+				frame.dummyTime = 100
+				frame:SetMinMaxValues(0, 100)
+				frame:SetValue(100)
+
+				frame:SetMovable(true)
+				frame.icon:EnableMouse(false)
+				frame.need:EnableMouse(false)
+				frame.greed:EnableMouse(false)
+				frame:Show()
+
+				configuring = true
+			else
+				frame:SetMovable(false)
+				frame.icon:EnableMouse(true)
+				frame.need:EnableMouse(true)
+				frame.greed:EnableMouse(true)
+				frame:Hide()
+
+				configuring = false
+			end
+		end
+	end
+end
+
 local Yatzee = CreateFrame('Frame')
 Yatzee:SetScript('OnEvent', function(self, event, ...) self[event](self, ...) end)
 Yatzee:RegisterEvent('VARIABLES_LOADED')
 
 function Yatzee:START_LOOT_ROLL(id, time)
+	if(configuring) then
+		for index, frame in pairs(frames) do
+			frame:Hide()
+		end
+	end
+
 	for index, frame in pairs(frames) do
 		if(not frame:IsShown()) then
+			active = true
+
 			frame.id = id
 			frame.time = time
 			frame:Show()
-
-			return
+			frame:SetMovable(false)
+			return OnShow(frame)
 		end
 	end
 end
+
+function Yatzee:PLAYER_REGEN_DISABLED()
+	if(configuring) then
+		for index, frame in pairs(frames) do
+			frame:Hide()
+		end
+
+		print('|cffff8080Yatzee:|r Entering combat, locking down frames.')
+		configuring = false
+	end
+end
+	
 
 function Yatzee:CONFIRM_LOOT_ROLL(id, type)
 	for index = 1, STATICPOPUP_NUMDIALOGS do
@@ -196,7 +287,11 @@ function Yatzee:CONFIRM_LOOT_ROLL(id, type)
 end
 
 function Yatzee:VARIABLES_LOADED()
+	SlashCmdList.Yatzee = SlashCommand
+	SLASH_Yatzee1 = '/yatzee'
+
 	self:UnregisterEvent('VARIABLES_LOADED')
+	self:RegisterEvent('PLAYER_REGEN_DISABLED')
 	self:RegisterEvent('START_LOOT_ROLL')
 
 	self:RegisterEvent('CONFIRM_LOOT_ROLL')
